bc0f6fd5db1529091c3cfe15bd9ad9c9c913629a
Nishit Mohan <nishitmohan@Nishits-MacBook-Air.local>
2021-02-03 20:57:16 +0530

    Changed to magento sites

---
+++ _data/sites/colorbar.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: "Magento", // optional, falls back to object key
+	description: "Magento web sites",
+	// skip if localhost
+	// skip if this is a new fork of the speedlify (not Zach’s)
+	//skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	options: {
+		frequency: 15, // 11h, 30m
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site",
+	},
+	urls: [
+		// "https://www.netlify.com/donation-matching/",
+		"https://www.colorbarcosmetics.com/",
+		"https://www.forevernew.co.in/",
+		"https://www.biotique.com",
+	]
+};
